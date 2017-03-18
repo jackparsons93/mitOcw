b6a98e4df6355133162c3e29591f3742d5f404a4
chris wilson <chriswilson@chriss-iMac.home>
2017-03-18 05:41:35 -0500

    Mit OCW Java 6.092

---
+++ GravityCalculator.java
@@ -0,0 +1,15 @@
+// mit OCW 6.092
+class GravityCalculator {
+	public static void main(String[] arguments) {
+		double gravity = -9.81; // Earth's gravity in m/s^2
+		double initialVelocity = 0.0;
+		double fallingTime = 10.0;
+		double initialPosition = 0.0;
+		double finalPosition = 0.5*gravity*Math.pow(fallingTime,2)+initialVelocity*fallingTime+initialPosition;
+ 		System.out.println("The object's position after " + fallingTime +
+		" seconds is " + finalPosition + " m.");
+	}
+}
+
+// First Output The object's position after 10.0 seconds is 0.0 m.
+// Second Output The object's position after 10.0 seconds is -490.5 m.
